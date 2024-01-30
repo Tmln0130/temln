4ea853bfbf3bb5535c4a11532b31669b9fb58064
DELL <DELL@172.16.153.100>
2024-01-30 16:24:38 +0800

    4.24pm

--- lab7/src/lab7/Dijkistra.java
+++ lab7/src/lab7/Dijkistra.java
@@ -1,32 +1,87 @@
 package lab7;
-import dataStructures.ArrayLinearList; 
+
+
 
 public class Dijkistra {
-	public static void main(String[]args) {
-		
-		graph thegraph = new graph(5);
-		thegraph.addEdge(0,1,6);
-		thegraph.addEdge(0,3,2);
-		thegraph.addEdge(1,3,2);
-		thegraph.addEdge(3,4,1);
-		thegraph.addEdge(0,1,6);
-		
-	}
+    public static void main(String[] args) {
+        // Матрицын хэмжээ
+        int M = 3; // A матрицын мөр
+        int K = 2; // A, B матрицын багана
+        int N = 3; // B матрицын мөр
+
+        // Матриц A, B болон үржвэр матриц C
+        int[][] A = {{1, 4}, {2, 5}, {3, 6}};
+        int[][] B = {{8, 7, 6}, {5, 4, 3}};
+        int[][] C = new int[M][N];
+
+        // Worker thread-үүдийн массив
+        Thread[] workers = new Thread[M * N];
+
+        // Матрицын үржвэр
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                C[i][j] = 0;
+
+                // Тухайн элементийн үржвэрт ашиглагддаг worker thread-ийг үүсгэнэ
+                workers[i * N + j] = new Thread(new WorkThread(i, j, A, B, C));
+
+                // Worker thread-ийг эхлүүлэх
+                workers[i * N + j].start();
+            }
+        }
+
+        // Бүх worker thread-ийг холбох
+        try {
+            for (Thread worker : workers) {
+                worker.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Матрицуудыг хэвлэх
+        System.out.println("Матриц A:");
+        printMatrix(A);
+
+        System.out.println("\nМатриц B:");
+        printMatrix(B);
+
+        System.out.println("\nМатриц C (A * B):");
+        printMatrix(C);
+    }
+
+    // Матрицыг хэвлэх функц
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
-class graph {
-	int[][]g;
-	ArrayLinearList visited = new ArrayLinearList();
-	ArrayLinearList unvisited = new ArrayLinearLIst();
-	ArrayLinearList table = new ArrayLinearList();
-	
-	public graph(int size)
-	{
-		g = new 
-	int [size][size];
-	for{int i=0; j<g.length; i++)
-		for (int j =0;j<g.length;j++)
-			g[i][j] = 0;
-	}
-	}
-			
+
+// Worker thread класс, тухайн элементийн үржүүлэхийн тулд Runnable интерфейс ашиглаж байна
+class WorkThread implements Runnable {
+    private int row;
+    private int col;
+    private int[][] A;
+    private int[][] B;
+    private int[][] C;
+
+    public WorkThread(int row, int col, int[][] A, int[][] B, int[][] C) {
+        this.row = row;
+        this.col = col;
+        this.A = A;
+        this.B = B;
+        this.C = C;
+    }
+
+    // Run метод нь тухайн worker thread-ээр хийгдэх үйлдлийг тодорхойлох функц
+    public void run() {
+        // C[row][col] дахь матрицын үржвэрийг тооцоолох
+        for (int k = 0; k < A[0].length; k++) {
+            C[row][col] += A[row][k] * B[k][col];
+        }
+    }
 }
