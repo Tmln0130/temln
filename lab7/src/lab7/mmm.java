8983ca5a64918c073d61bca39d88b597c2f1b38d
DELL <DELL@172.16.155.2>
2024-03-26 12:28:59 +0800

    ad

--- lab7/src/lab7/mmm.java
+++ lab7/src/lab7/mmm.java
@@ -1,198 +1,203 @@
-import numpy as np
+package lab7;
+import java.util.*;
 
-from turtle import distance  
+class Problem {
+    String initial;
+    String goal;
 
-class Problem:
-    def __init__(self, initial, goal):
-        self.initial = initial
-        self.goal = goal
+    public Problem(String initial, String goal) {
+        this.initial = initial;
+        this.goal = goal;
+    }
 
-    def actions(self, state):
-        """Return the possible actions from the given state."""
-        raise NotImplementedError("Subclasses must implement actions method")
+    List<String> actions(String state) {
+        throw new UnsupportedOperationException("Subclasses must implement actions method");
+    }
 
-    def result(self, state, action):
-        """Return the state that results from taking the given action in the given state."""
-        raise NotImplementedError("Subclasses must implement result method")
+    String result(String state, String action) {
+        throw new UnsupportedOperationException("Subclasses must implement result method");
+    }
 
-    def goal_test(self, state):
-        """Return True if the state is a goal state."""
-        return state == self.goal
+    boolean goalTest(String state) {
+        return state.equals(goal);
+    }
 
-    def path_cost(self, cost_so_far, state1, action, state2):
-        """Return the cost of the path from the initial state to state2 via action."""
-        return cost_so_far + 1  # Default cost is 1 for each step
-    
+    int pathCost(int costSoFar, String state1, String action, String state2) {
+        return costSoFar + 1;  // Default cost is 1 for each step
+    }
+}
 
-class Node:
-    def __init__(self, state, parent=None, action=None, path_cost=0):
-        self.state = state  # The state in the state space to which the node corresponds
-        self.parent = parent  # The node in the search tree that generated this node
-        self.action = action  # The action that was applied to the parent to generate this node
-        self.path_cost = path_cost  # The cost of the path from the initial state to this node
+class Node {
+    String state;
+    Node parent;
+    String action;
+    int pathCost;
 
-    def expand(self, problem):
-        """Return a list of nodes reachable from this node."""
-        return [self.child_node(problem, action) for action in problem.actions(self.state)]
+    Node(String state, Node parent, String action, int pathCost) {
+        this.state = state;
+        this.parent = parent;
+        this.action = action;
+        this.pathCost = pathCost;
+    }
 
-    def child_node(self, problem, action):
-        """Return a child node from this node given an action."""
-        next_state = problem.result(self.state, action)
-        next_path_cost = problem.path_cost(self.path_cost, self.state, action, next_state)
-        return Node(next_state, self, action, next_path_cost)
+    List<Node> expand(Problem problem) {
+        List<Node> nodes = new ArrayList<>();
+        for (String action : problem.actions(state)) {
+            nodes.add(childNode(problem, action));
+        }
+        return nodes;
+    }
 
-    def solution(self):
-        """Return the sequence of actions to go from the root to this node."""
-        return [node.action for node in self.path()[1:]]
+    Node childNode(Problem problem, String action) {
+        String nextState = problem.result(state, action);
+        int nextPathCost = problem.pathCost(pathCost, state, action, nextState);
+        return new Node(nextState, this, action, nextPathCost);
+    }
 
-    def path(self):
-        """Return a list of nodes forming the path from the root to this node."""
-        node, path_back = self, []
-        while node:
-            path_back.append(node)
-            node = node.parent
-        return list(reversed(path_back))
+    List<String> solution() {
+        List<String> actions = new ArrayList<>();
+        for (Node node : path()) {
+            if (node.action != null) {
+                actions.add(node.action);
+            }
+        }
+        return actions;
+    }
 
+    List<Node> path() {
+        List<Node> pathBack = new ArrayList<>();
+        Node node = this;
+        while (node != null) {
+            pathBack.add(node);
+            node = node.parent;
+        }
+        Collections.reverse(pathBack);
+        return pathBack;
+    }
+}
 
-from collections import deque
+class Graph {
+    Map<String, Map<String, Integer>> graphDict;
+    boolean directed;
 
-class Graph:
-    def __init__(self, graph_dict=None, directed=True):
-        self.graph_dict = graph_dict or {}
-        self.directed = directed
-        if not directed:
-            self.make_undirected()
+    Graph(Map<String, Map<String, Integer>> graphDict, boolean directed) {
+        this.graphDict = graphDict != null ? graphDict : new HashMap<>();
+        this.directed = directed;
+        if (!directed) {
+            makeUndirected();
+        }
+    }
 
-    def make_undirected(self):
-        for a in list(self.graph_dict.keys()):
-            for (b, dist) in self.graph_dict[a].items():
-                self.connect1(b, a, dist)
+    void makeUndirected() {
+        for (String a : new ArrayList<>(graphDict.keySet())) {
+            for (Map.Entry<String, Integer> entry : graphDict.get(a).entrySet()) {
+                connect1(entry.getKey(), a, entry.getValue());
+            }
+        }
+    }
 
-    def connect(self, A, B, distance=1):
-        self.connect1(A, B, distance)
-        if not self.directed:
-            self.connect1(B, A, distance)
+    void connect(String A, String B, int distance) {
+        connect1(A, B, distance);
+        if (!directed) {
+            connect1(B, A, distance);
+        }
+    }
 
-    def connect1(self, A, B, distance):
-        self.graph_dict.setdefault(A, {})[B] = distance
+    void connect1(String A, String B, int distance) {
+        graphDict.computeIfAbsent(A, k -> new HashMap<>()).put(B, distance);
+    }
 
-    def get(self, a, b=None):
-        if b is None:
-            return self.graph_dict.get(a, {})
-        else:
-            return self.graph_dict.get(a, {})
-            
+    Map<String, Integer> get(String a) {
+        return graphDict.getOrDefault(a, new HashMap<>());
+    }
 
+    List<String> nodes() {
+        Set<String> nodes = new HashSet<>(graphDict.keySet());
+        for (Map<String, Integer> map : graphDict.values()) {
+            nodes.addAll(map.keySet());
+        }
+        return new ArrayList<>(nodes);
+    }
+}
 
-    def nodes(self):
-        s1 = set([k for k in self.graph_dict.keys()])
-        s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
-        nodes = s1.union(s2)
-        return list(nodes)
+class GraphProblem extends Problem {
+    Graph graph;
 
-def breadth_first_graph_search(problem):
-    # Breadth-first graph search
-    node = Node(problem.initial)
-    if problem.goal_test(node.state):
-        return node
-    frontier = deque([node])
-    explored = set()
-    while frontier:
-        node = frontier.popleft()
-        explored.add(node.state)
-        for child in node.expand(problem):
-            if child.state not in explored and child not in frontier:
-                if problem.goal_test(child.state):
-                    return child
-                frontier.append(child)
-    return None
+    GraphProblem(String initial, String goal, Graph graph) {
+        super(initial, goal);
+        this.graph = graph;
+    }
 
-def depth_first_graph_search(problem):
-    # Depth-first graph search
-    frontier = [(Node(problem.initial))] # Stack
-    explored = set()
-    while frontier:
-        node = frontier.pop()
-        if problem.goal_test(node.state):
-            return node
-        explored.add(node.state)
-        frontier.extend(child for child in node.expand(problem) if child.state not in explored and child not in frontier)
-    return None
+    List<String> actions(String A) {
+        return new ArrayList<>(graph.get(A).keySet());
+    }
 
-def UndirectedGraph(graph_dict=None):
-    return Graph(graph_dict=graph_dict, directed=False)
+    String result(String state, String action) {
+        return action;
+    }
 
-romania_map = UndirectedGraph({
-    'Arad': {'Zerind': 75, 'Sibiu': 140, 'Timisoara': 118},
-    'Bucharest': {'Urziceni': 85, 'Pitesti': 101, 'Giurgiu': 90, 'Fagaras': 211},
-    'Craiova': {'Drobeta': 120, 'Rimnicu': 146, 'Pitesti': 138},
-    'Drobeta': {'Mehadia': 75},
-    'Eforie': {'Hirsova': 86},
-    'Fagaras': {'Sibiu': 99},
-    'Hirsova': {'Urziceni': 98},
-    'Iasi': {'Vaslui': 92, 'Neamt': 87},
-    'Lugoj': {'Timisoara': 111, 'Mehadia': 70},
-    'Oradea': {'Zerind': 71, 'Sibiu': 151},
-    'Pitesti': {'Rimnicu': 97},
-    'Rimnicu': {'Sibiu': 80},
-    'Urziceni': {'Vaslui': 142}
-})
+    int pathCost(int costSoFar, String A, String action, String B) {
+        return costSoFar + (graph.get(A).getOrDefault(B, Integer.MAX_VALUE));
+    }
 
-class GraphProblem(Problem):
-    def __init__(self, initial, goal, graph):
-        super().__init__(initial, goal)
-        self.graph = graph
-    
-    def actions(self, A):
-        return list(self.graph.get(A).keys())
-    
-    def result(self, state, action):
-        return action
-    
-    def path_cost(self, cost_so_far, A, action, B):
-        return cost_so_far + (self.graph.get(A, {}).get(B) or np.inf)
-    
-    def find_min_edge(self):
-        m = np.inf
-        for d in self.graph.graph_dict.values():
-            local_min = min(d.values())
-            m = min(m, local_min)
-        return m
-    
-    def h(self, node):
-        locs = getattr(self.graph, 'locations', None)
-        if locs:
-            if type(node) is str:
-                return int(distance(locs[node], locs[self.goal]))
-            return int(distance(locs[node.state], locs[self.goal]))
-        else:
-            return np.inf
-        
-class GraphProblemStochastic(GraphProblem):
-    def __init__(self, initial, goal, graph):
-        super().__init__(initial, goal, graph)  # Passing graph to the superclass constructor
-       
-    def actions(self, A):
-        return list(self.graph.get(A).keys())
-    
-    def result(self, state, action):
-        return action  # Assuming action is directly the resulting state
-    
-    def path_cost(self, cost_so_far, A, action, B):
-        raise NotImplementedError("path_cost method must be implemented in subclasses")
+    int findMinEdge() {
+        int minDistance = Integer.MAX_VALUE;
+        for (Map<String, Integer> d : graph.graphDict.values()) {
+            int localMin = d.values().stream().min(Comparator.naturalOrder()).orElse(Integer.MAX_VALUE);
+            minDistance = Math.min(minDistance, localMin);
+        }
+        return minDistance;
+    }
 
+    int h(Node node) {
+        Map<String, Integer> locs = graph.graphDict.get("locations");
+        if (locs != null) {
+            if (node.state instanceof String) {
+                // Assuming "distance" method calculates distance between two points
+                return (int) distance(locs.get(node.state), locs.get(goal));
+            }
+            // Assuming "distance" method calculates distance between two points
+            return (int) distance(locs.get(node.state), locs.get(goal));
+        } else {
+            return Integer.MAX_VALUE;
+        }
+    }
+}
 
-def GraphSearch():
-    problem = GraphProblem('Arad', 'Bucharest', romania_map)
-    searcher = breadth_first_graph_search
+class GraphProblemStochastic extends GraphProblem {
+    GraphProblemStochastic(String initial, String goal, Graph graph) {
+        super(initial, goal, graph);
+    }
 
-    def do(searcher, problem):
-        goal_node = searcher(problem)
-        print('Search algorithm:', searcher.__name__)
-        print('List of nodes visited:', goal_node.solution())
-        print('Path cost:', goal_node.path_cost)  # Access path cost attribute directly
+    List<String> actions(String A) {
+        return new ArrayList<>(graph.get(A).keySet());
+    }
 
+    String result(String state, String action) {
+        return action;  // Assuming action is directly the resulting state
+    }
 
-    do(searcher, problem)
+    int pathCost(int costSoFar, String A, String action, String B) {
+        throw new UnsupportedOperationException("pathCost method must be implemented in subclasses");
+    }
+}
 
-GraphSearch()
+class RomaniaMap {
+    static Graph UndirectedGraph() {
+        Map<String, Map<String, Integer>> graphDict = new HashMap<>();
+        graphDict.put("Arad", Map.of("Zerind", 75, "Sibiu", 140, "Timisoara", 118));
+        graphDict.put("Bucharest", Map.of("Urziceni", 85, "Pitesti", 101, "Giurgiu", 90, "Fagaras", 211));
+        graphDict.put("Craiova", Map.of("Drobeta", 120, "Rimnicu", 146, "Pitesti", 138));
+        graphDict.put("Drobeta", Map.of("Mehadia", 75));
+        graphDict.put("Eforie", Map.of("Hirsova", 86));
+        graphDict.put("Fagaras", Map.of("Sibiu", 99));
+        graphDict.put("Hirsova", Map.of("Urziceni", 98));
+        graphDict.put("Iasi", Map.of("Vaslui", 92, "Neamt", 87));
+        graphDict.put("Lugoj", Map.of("Timisoara", 111, "Mehadia", 70));
+        graphDict.put("Oradea", Map.of("Zerind", 71, "Sibiu", 151));
+        graphDict.put("Pitesti", Map.of("Rimnicu", 97));
+        graphDict.put("Rimnicu", Map.of("Sibiu", 80));
+        graphDict.put("Urziceni", Map.of("Vaslui", 142));
+        return new Graph(graphDict, false);
+    }
+}
